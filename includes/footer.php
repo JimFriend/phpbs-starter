d1bf61f2b68d825aadc5e3639efb20be9c8388e3
Jim Friend <jamesfriend@James-Friends-MacBook-Pro.local>
2013-12-26 15:43:50 -0800

    Initial commit of twitter bootstrap files and base jumbotron template

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+			<hr>
+	
+			<footer>
+				<p>&copy; Company 2013</p>
+			</footer>
+		</div> <!-- /container -->
+	
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+	</body>
+</html>
